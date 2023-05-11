2c5cc9812c9ac7372e1d76e042adac6568a8ba1a
WescAlves <wesley.c.alves98@gmail.com>
2023-05-11 17:07:29 -0300

    Criando funções para os botões adicionados.

--- Exercício 2/script.js
+++ Exercício 2/script.js
@@ -4,6 +4,14 @@ const form = document.querySelector("form");
 let lista = document.querySelector("ul");
 let btn = document.querySelector("#btn");
 let array = [];
+let funcSacar = function(saldo){
+    saldo = parseFloat(saldo-100);
+    return saldo;
+}
+let funcDepositar = function(saldo){
+    saldo = parseFloat(saldo+100);
+    return saldo;
+}
 class clienteBanco  {
     constructor(nome, documento, saldo){
         this.nome = nome;
@@ -28,5 +36,21 @@ btn.addEventListener("click", function(){
         buttonDepositar.textContent = "Depositar 100";
         novoItem.appendChild(buttonSacar);
         novoItem.appendChild(buttonDepositar);
+        buttonSacar.addEventListener("click", function(){
+            saldo = funcSacar(cliente.saldo);
+            cliente.saldo = saldo;
+            console.log(saldo);
+            novoItem.textContent = `Cliente: ${cliente.nome} | Documento: ${cliente.documento} | Saldo: ${cliente.saldo}`
+            novoItem.appendChild(buttonSacar);
+            novoItem.appendChild(buttonDepositar);
+        })
+        buttonDepositar.addEventListener("click", function(){
+            saldo = funcDepositar(cliente.saldo);
+            cliente.saldo = saldo;
+            console.log(saldo);
+            novoItem.textContent = `Cliente: ${cliente.nome} | Documento: ${cliente.documento} | Saldo: ${cliente.saldo}`
+            novoItem.appendChild(buttonSacar);
+            novoItem.appendChild(buttonDepositar);
+        })
     });
 })
